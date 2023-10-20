afe46f0f64b16272e29483835fa8d6030afb76a9
sbai.houda <houdasbai67@gmail.com>
2023-10-20 14:09:40 +0200

    added contact form and users list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductslistComponent } from './productslist/productslist.component';
 import { OrderslistComponent } from './orderslist/orderslist.component';
 import { UserslistComponent } from './userslist/userslist.component';
+import { ContactformComponent } from './contactform/contactform.component';
 
 const routes: Routes = [
   { path: 'productslist', component: ProductslistComponent },
   { path: 'orderslist', component: OrderslistComponent },
-  { path :'userslist' , component: UserslistComponent}
+  { path :'userslist' , component: UserslistComponent},
+  { path : 'contactform', component: ContactformComponent}
 
 ];
 @NgModule({
--- src/app/contactform/contactform.component.ts
+++ src/app/contactform/contactform.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contactform',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./contactform.component.css']
 })
 export class ContactformComponent {
+  form = new FormGroup({
+    Name: new FormControl(''),
+    Email: new FormControl(''),
+    Message: new FormControl('')
+  });
 
+  onSubmit(){
+    console.warn(this.form.value);
+  }
 }
